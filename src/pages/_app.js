a70db9c4f116789642745316c0911d1b59ba9cb6
Ad7Store <ad7store.log@gmail.com>
2025-12-29 14:46:39 +0500

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,29 @@
-import { CartProvider } from '@/contexts/CartContext'
 import { AuthProvider } from '@/contexts/AuthContext'
-import '@/styles/globals.css'
+import { CartProvider } from '@/contexts/CartContext'
 import { Toaster } from 'react-hot-toast'
+import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
   return (
     <AuthProvider>
       <CartProvider>
-        <Toaster position="top-right" />
+        <Toaster 
+          position="top-right"
+          toastOptions={{
+            duration: 3000,
+            style: {
+              background: '#363636',
+              color: '#fff',
+            },
+            success: {
+              duration: 3000,
+              theme: {
+                primary: 'green',
+                secondary: 'black',
+              },
+            },
+          }}
+        />
         <Component {...pageProps} />
       </CartProvider>
     </AuthProvider>
